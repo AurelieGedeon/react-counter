77733bb7bfb4c9fc0db3396ead548abb3a20cfc2
Aurelie Gedeon <aureliegedeon@Aurelies-Macbook-Pro.local>
2022-02-14 11:46:59 -0500

    added buttons to coffee list

--- src/Coffees.js
+++ src/Coffees.js
@@ -2,15 +2,21 @@ import React, { useState, useEffect } from "react";
 
 function Coffees() {
   const [coffeeList, setCoffeeList] = useState();
+  const [temperature, setTemperature] = useState("hot");
   useEffect(() => {
-    fetch("https://api.sampleapis.com/coffee/hot")
+    fetch(`https://api.sampleapis.com/coffee/${temperature}`)
       .then((response) => response.json())
       .then((data) => setCoffeeList(data))
       .catch((error) => console.error(error));
-  }, []);
+  }, [temperature]);
   return (
     <section className="coffee-container" id="coffees">
+      {/* We need our JSX to be inside a SINGLE CONATINING TAG. That tag can be a React.Fragment <></> if we don't want any HTML to render  */}
       <h2>Coffee List</h2>
+      <div className="button-container">
+        <button onClick={() => setTemperature("hot")}>Hot</button>
+        <button onClick={() => setTemperature("iced")}>Iced</button>
+      </div>
       {!coffeeList ? (
         <h3> Loading Recipes...</h3>
       ) : (
